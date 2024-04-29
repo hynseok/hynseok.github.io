e06e9457fc911c71d57a163a7c7f6aff96b0dcbb
hynseok <wefwef12e@naver.com>
2024-04-30 00:08:06 +0900

    설정 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1714401611',resources: [
+    cacheName: 'chirpy-1714403008',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
@@ -16,8 +16,6 @@ const swconf = {
       
       
     ],allowHosts: [
-      
-        'chirpy-img.netlify.app',
       
           'fonts.googleapis.com',
           'fonts.gstatic.com',
